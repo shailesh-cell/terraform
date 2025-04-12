f2ee0645e7ff2c257cb5ec8ea2f94ce19874ca14
Shail <s.c1@mail.com>
2025-04-12 15:47:12 +0530

    43

---
+++ ahub-dev-cus/local.tf
@@ -0,0 +1,24 @@
+module "globals" {
+ source = "../_modules/global-variables"
+}
+
+locals {
+ address_space = "10.101.12.0/22"
+ environemnt   = "dev"
+ product       = "ahub"
+ resource_groups = [
+ "app",
+ "backup",
+ "data",
+ "monitor",
+ "network",
+ "security",
+ "vm"
+]
+
+tags = {
+ "CreatedBy"    =  "ITops-GIT-Pipeline
+ "CreatedOn"    =  formatdate("MM/DD/YYYY", timestamp())
+ "DeploymentType" = "Terraform"
+ "Environment"   = local.environment == "prod" ? "Production"  : local.environment == "nonprod" ? "Non-Prrodution" : title(local.environment)
+ "Product"       = upper(local.product)
