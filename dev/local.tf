2e02c7a4b289e4cd90d283306964b3c7f76c1813
Shail <s.c1@mail.com>
2025-04-12 17:51:34 +0530

    58

--- dev/local.tf
+++ dev/local.tf
@@ -5,7 +5,7 @@ module "globals" {
 locals {
  address_space = "10.101.12.0/22"
  environemnt   = "dev"
- product       = "ahub"
+ product       = "myproduct"
  resource_groups = [
  "app",
  "backup",
@@ -16,11 +16,13 @@ locals {
  "vm"
 ]
 
-tags = {
- "CreatedBy"    =  "GIT-Pipeline
- "CreatedOn"    =  formatdate("MM/DD/YYYY", timestamp())
- "DeploymentType" = "Terraform"
- "Environment"   = local.environment == "prod" ? "Production"  : local.environment == "nonprod" ? "Non-Prrodution" : title(local.environment)
- "Product"       = upper(local.product)
-}
+  tags = {
+    CreatedBy      = "GIT-Pipeline"
+    CreatedOn      = formatdate("MM/DD/YYYY", timestamp())
+    DeploymentType = "Terraform"
+    Environment    = local.environment == "prod" ? "Production" :
+                     local.environment == "nonprod" ? "Non-Production" :
+                     title(local.environment)
+    Product        = upper(local.product)
+  }
 }
