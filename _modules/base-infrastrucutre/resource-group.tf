9155c19497a69b941d2834cd063fa98e3b9f9adb
Shail <s.c1@mail.com>
2025-04-12 14:59:18 +0530

    40

--- _modules/base-infrastrucutre/resource-group.tf
+++ _modules/base-infrastrucutre/resource-group.tf
@@ -4,7 +4,7 @@ Description: Resource group.
 
 resource "azurerm_resource_group" "rg" {
  for_each  = toset(var.resource_groups)
- name = "$(var.product)-$()var.environment)-$(each.value)-rg-$(module.globals.regions[var.location])"
+ name = "$(var.product)-$(var.environment)-$(each.value)-rg-$(module.globals.regions[var.location])"
  location = module.globals.locations[var.location]
  tags  = var.tags
 
