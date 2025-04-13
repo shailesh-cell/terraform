7feadd5139ac8d5f4097a6083f4624564c990285
Shail <s.c1@mail.com>
2025-04-13 17:54:27 +0530

    60

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,10 @@
 module "base_infra" {
   source   = "../_modules/base-infrastructure"
-  resource_groups = ["{local.product}-${local.environment}-${rg}-${module.globals.regions["primary"]}-rg"]
+  resource_groups = [
+  for rg in local.resource_groups :
+  "${local.product}-${local.environment}-${rg}-${module.globals.regions[local.location]}-rg"
+]
+
   environment = local.environment
   location    = "primary"
   product    = local.product
