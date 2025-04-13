88a7e737252c89f739f37a29dc9a26ab1ca97689
Shail <s.c1@mail.com>
2025-04-13 14:16:59 +0530

    60

---
+++ dev/locals.tf
@@ -0,0 +1,26 @@
+locals {
+  environment     = "dev"
+  address_space   = "10.101.12.0/22"
+  product         = "myproduct"
+  resource_groups = [
+    "app",
+    "backup",
+    "data",
+    "monitor",
+    "network",
+    "security",
+    "vm"
+  ]
+
+  tags = {
+    CreatedBy      = "GIT-Pipeline"
+    CreatedOn      = formatdate("MM/DD/YYYY", timestamp())
+    DeploymentType = "Terraform"
+    Environment    = (
+      local.environment == "prod" ? "Production" :
+      local.environment == "uat" ? "Non-Production" :
+      title(local.environment)
+    )
+    Product        = upper(local.product)
+  }
+}
