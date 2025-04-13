0a1524ca250d0f6c25371c1a092bd54de22e17ea
Shail <s.c1@mail.com>
2025-04-13 14:49:25 +0530

    60

--- dev/main.tf
+++ dev/main.tf
@@ -11,4 +11,8 @@ module "global_vars" {
 module "base_infra" {
   source   = "../_modules/base-infrastructure"
   for_each = toset(local.resource_groups)
+  resource_groups = "$(var.product)-$(var.environment)-$(each.value)-rg-${module.globals.regions[var.location]}"
+  environment = local.environment
+  location    = local.location
+  tags     = local.tags
 }
