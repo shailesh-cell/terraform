b9725f49af7ff634558bff195204dc5f5553c858
Shail <s.c1@mail.com>
2025-04-13 14:50:29 +0530

    60

--- dev/main.tf
+++ dev/main.tf
@@ -2,12 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-module "global_vars" {
-  source      = "../_modules/global-variables"
-  environment = local.environment
-  location    = local.location
-}
-
 module "base_infra" {
   source   = "../_modules/base-infrastructure"
   for_each = toset(local.resource_groups)
