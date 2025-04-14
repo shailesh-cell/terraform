c3c5950f5bd17f402ab867b033d425b7ba1f70ec
Shail <s.c1@mail.com>
2025-04-15 00:12:36 +0530

    vm-16

--- dev/rg.tf
+++
@@ -1,8 +0,0 @@
-module "resourcegroup" {
-  source = "../_modules/base-infrastructure"
-
-  resource_groups = local.resource_groups
-  product         = var.product
-  environment     = var.environment
-  location        = var.location
-}
