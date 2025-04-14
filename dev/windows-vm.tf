c1556774a8d90385082261b5dadab77f3de87e77
Shail <s.c1@mail.com>
2025-04-14 22:21:22 +0530

    vm-4

--- dev/windows-vm.tf
+++ dev/windows-vm.tf
@@ -5,6 +5,6 @@ module "windows_vm" {
   location    = "primary"
   product    = local.product
   address_space = local.address_space
-  subnet        = local.subnet
+  subnets        = local.subnet
   tags     = local.tags
 }
