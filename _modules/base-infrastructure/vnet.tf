d62b21e671d79e3b2a5700f82fd79000469ef1e4
Shail <s.c1@mail.com>
2025-04-14 22:55:00 +0530

    vm-9

--- _modules/base-infrastructure/vnet.tf
+++ _modules/base-infrastructure/vnet.tf
@@ -22,7 +22,7 @@ resource "azurerm_network_interface" "nic" {
   for_each            = var.subnets
   name                = each.key
   location            = module.globals.locations[var.location]
-  resource_group_name = azurerm_resource_group.rg["network"].name
+  resource_group_name = azurerm_resource_group.rg["vm"].name
   tags  = var.tags
   
   ip_configuration {
