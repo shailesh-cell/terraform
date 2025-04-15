a5814aa117629b3ffcf243b1477289bd0b13da08
Shail <s.c1@mail.com>
2025-04-15 14:23:14 +0530

    vm-42

--- dev/windows-vm.tf
+++ dev/windows-vm.tf
@@ -5,6 +5,6 @@ module "windows_vm" {
   address_space = local.address_space
   resource_groups = ["vm"]
   tags           = local.tags
-  nic_id         = "var.nic_id"
+  nic_id         = azurerm_network_interface.nic.id
 }
 
