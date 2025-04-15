bb0f48686baa845d4895d18f8a9e40adf7bb6d85
Shail <s.c1@mail.com>
2025-04-15 14:09:02 +0530

    vm-40

--- dev/base-infra.tf
+++ dev/base-infra.tf
@@ -1,10 +1,10 @@
-module "windows_vm" {
-  source   = "../_modules/windows-virtual-machine"
+module "base_infra" {
+  source = "../_modules/base-infrastructure"
   environment  = local.environment
   subnets      = local.subnets
   address_space = local.address_space
   resource_groups = ["vm"]
   tags           = local.tags
   nic_id         = "var.nic_id"
+  resource_groups = ["vm","network"]
 }
-
