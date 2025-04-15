aea6b4326235d5c1b362e82e45b31f0cdef7d331
Shail <s.c1@mail.com>
2025-04-15 12:59:48 +0530

    vm-21

--- _modules/base-infrastructure/output.tf
+++ _modules/base-infrastructure/output.tf
@@ -10,14 +10,7 @@ output "vnet"  {
  value = azurerm_virtual_network.vnet
 }
 
-output "subnet_ids" {
-  value = {
-    for k, s in azurerm_subnet.subnet : k => s.id
-  }
-}
-
-output "network_rg_name" {
-  value = azurerm_resource_group.rg["network"].name
-}
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
 
 
