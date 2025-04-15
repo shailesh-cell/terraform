e38d34727249bf94dadd75692196b63ed74f44da
Shail <s.c1@mail.com>
2025-04-15 13:35:59 +0530

    vm-30

--- dev/windows-vm.tf
+++ dev/windows-vm.tf
@@ -1,11 +1,4 @@
 module "windows_vm" {
   source   = "../_modules/windows-virtual-machine"
-  resource_groups = ["vm"]
-  environment = local.environment
-  location    = "primary"
-  product    = local.product
-  address_space = local.address_space
-  subnets        = local.subnets
-  tags     = local.tags
 }
 
