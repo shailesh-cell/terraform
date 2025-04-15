26658df43ef9c02a9b06cfb4b9fda3ed454b9be0
Shail <s.c1@mail.com>
2025-04-15 14:00:56 +0530

    vm-38

--- dev/windows-vm.tf
+++ dev/windows-vm.tf
@@ -5,6 +5,6 @@ module "windows_vm" {
   address_space = local.address_space
   resource_groups = ["vm"]
   tags           = local.tags
-  nic_id         = var.nic_id
+  nic_id         = "var.nic_id"
 }
 
