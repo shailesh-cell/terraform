6cdf73220d8efaf5ecbd1a36a5e723493e94222c
Shail <s.c1@mail.com>
2025-04-15 13:29:30 +0530

    vm-27

--- _modules/windows-virtual-machine/main.tf
+++ _modules/windows-virtual-machine/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_windows_virtual_machine" "windows_vm" {
   name                = "${var.product}-${var.environment}-${module.globals.regions[var.location]}-vm"
-  resource_group_name = ${var.product}-${var.environment}-vm-${module.globals.regions[var.location]}-rg"
+  resource_group_name = "${var.product}-${var.environment}-vm-${module.globals.regions[var.location]}-rg"
   location            = module.globals.locations[var.location]
   size                = var.windows_vm_size
-  admin_username      = var.admin_username #"adminuser"
-  admin_password      = var.admin_password #"P@$$w0rd1234!"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
   network_interface_ids = [var.nic_id]
 
   os_disk {
