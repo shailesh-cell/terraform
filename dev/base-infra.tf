2f3072275b3b7e8f3132856c73a130b8e967f064
Shail <s.c1@mail.com>
2025-04-15 16:59:39 +0530

    vm-62

--- dev/base-infra.tf
+++ dev/base-infra.tf
@@ -1,7 +1,7 @@
 module "base_infra" {
   source = "../_modules/base-infrastructure"
   environment  = local.environment
-  subnets      = local.subnets
+  subnets      = ["network"]
   address_space = local.address_space
   resource_groups = local.resource_groups
   tags           = local.tags
