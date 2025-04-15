7e87037b2beb295631530e1bb66a6459644e6acc
Shail <s.c1@mail.com>
2025-04-15 14:53:25 +0530

    vm-47

--- dev/base-infra.tf
+++ dev/base-infra.tf
@@ -3,6 +3,6 @@ module "base_infra" {
   environment  = local.environment
   subnets      = local.subnets
   address_space = local.address_space
-  resource_groups = ["vm"]
+  resource_groups = local.resource_groups
   tags           = local.tags
 }
